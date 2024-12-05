a0c768e3475b8cce1086e903a010912057606801
Cristian Caballero Sanchez <cricabsan2@alum.us.es>
2024-12-05 10:50:37 +0100

    Update csvFormater.jl
    Parses correctly and merges Date and Time into DateTime for easier access

--- src/utils/csvFormater.jl
+++ src/utils/csvFormater.jl
@@ -1,11 +1,30 @@
 using CSV
 using DataFrames
+using Dates
 
-data_name = "household_power_consumption.txt"
+#= 
+    Takes a .txt file with at least Date and Time columns and outputs a .csv file. 
+    The Date and Time columns are merged into a single DateTime column in the resulting file.
+=#
 
+# Get the data
+data_name = "prueba.txt"
 resources_dir = joinpath(@__DIR__, "..", "..", "resources")
 data_path = joinpath(resources_dir, data_name)
 
+# Read the data
 data = CSV.read(data_path, DataFrame; delim = ";")
+print(data.Time[1:3])
+dates = Date.(data.Date, "dd/MM/yyyy")
+times = data.Time
 
+# Creates and adds the new column as the first one
+dateTimes = dates .+ times
+data[!, :DateTime] = dateTimes
+data = select(data, :DateTime, Not(:DateTime))
+
+# Remove the original Date and Time columns
+select!(data, Not([:Date, :Time]))
+
+# Write the modified DataFrame to a CSV file
 CSV.write("$resources_dir/data.csv", data)
